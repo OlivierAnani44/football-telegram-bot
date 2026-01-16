1687ad39131407d9f260590186179d7d7dbaa2fa
Olivier Anani <ananiolivier44@gmail.com>
2026-01-16 06:24:07 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from telegram import Bot
 import os
+import asyncio
+from telegram import Bot
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 
 bot = Bot(token=BOT_TOKEN)
-bot.send_message(chat_id=CHANNEL_ID, text="🚀 Test bot Railway OK !")
+
+async def main():
+    await bot.send_message(chat_id=CHANNEL_ID, text="🚀 Test bot Railway OK !")
+
+# Exécute la coroutine
+asyncio.run(main())
+
 
 
 
